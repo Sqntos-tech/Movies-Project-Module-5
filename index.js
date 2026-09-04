1d04b3e385dce29f7ad79101ed07d5b4874bcbeb
Sqntos-tech <edwardsantos1237@gmail.com>
2026-09-03 20:42:35 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const movieWrapper = document.querySelector(".movie")
+const nameWrapper = document.querySelector(".searchName")
 
 async function getMovies(searchTerm) {
     const response = await fetch(
         `http://www.omdbapi.com/?apikey=75b38caf&s=${searchTerm}`
     );
+    nameWrapper.innerHTML = searchTerm
     const data = await response.json();
     console.log(data.Search)
     movieWrapper.innerHTML = data.Search.map((movie) => {
@@ -13,7 +15,7 @@ async function getMovies(searchTerm) {
         <h4 class="movie__year">${movie.Year}</h4>
         <button class="movie__btn">Learn more</button>
         </div>`
-    })
+    }) .slice(0, 6).join("")
 }
 
 
