246c465793f519a35c087cc5637321d5309dbaf1
Sqntos-tech <edwardsantos1237@gmail.com>
2026-09-16 20:25:21 -0400

    added loading state

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const movieWrapper = document.querySelector(".movies")
 const nameWrapper = document.querySelector(".searchName")
 
 async function getMovies(searchTerm) {
-    movieWrapper.classList.add("movie__loading")
+    movieWrapper.innerHTML = `
+        <i class="fas fa-spinner movie__loading--spinner"></i>
+    `;
+
+    movieWrapper.classList.add("movie__loading");
+
     const response = await fetch(
         `https://www.omdbapi.com/?apikey=75b38caf&s=${searchTerm}`
     );
-    nameWrapper.innerHTML = searchTerm
+
     const data = await response.json();
+    nameWrapper.innerHTML = searchTerm
+
     movieWrapper.classList.remove("movie__loading")
-    console.log(data.Search)
-    movieWrapper.innerHTML = data.Search.map((movie) => {
+    
+    movieWrapper.innerHTML = data.Search
+    .map((movie) => {
         return `<div class='movie'>
         <img src='${movie.Poster}' class="movie__img" alt="Poster" />
         <h2 class="movie__title">${movie.Title}</h2>
